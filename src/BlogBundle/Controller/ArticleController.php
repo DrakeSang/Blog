90c72e10820b4c85069e6fa5a40addaf42144ad1
DrakeSang <emicha@mail.bg>
2019-01-16 21:19:57 +0200

    Adding the create article process

--- src/BlogBundle/Controller/ArticleController.php
+++ src/BlogBundle/Controller/ArticleController.php
@@ -2,17 +2,43 @@
 
 namespace BlogBundle\Controller;
 
+use BlogBundle\Entity\Article;
+use BlogBundle\Form\ArticleType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends Controller
 {
     /**
-     * @Route("/article/create", name="article_create")
+     * @param Request $request
+     *
      * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Exception
+     *
+     * @Route("/article/create", name="article_create")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     *
      */
-    public function createArticle()
+    public function createArticle(Request $request)
     {
-        return $this->render('article/create.html.twig');
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $currentUser = $this->getUser();
+            $article->setAuthor($currentUser);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute("blog_index");
+        }
+
+        return $this->render('article/create.html.twig',
+            array('form' => $form->createView()));
     }
 }
